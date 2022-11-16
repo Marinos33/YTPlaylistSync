4801af0a1236fbaa9b10388092d675992ab97120
Marinos33 <loicmidavaine@gmail.com>
2022-11-16 17:13:48 +0100

    added permission storage for 11+

--- app/src/main/java/com/marinos33/ytplaylistsync/MainActivity.kt
+++ app/src/main/java/com/marinos33/ytplaylistsync/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.marinos33.ytplaylistsync
 
-import android.content.ContentValues.TAG
+import android.Manifest.permission
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -37,6 +45,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        requestPermissions()
 
         initYoutubeDL()
 
@@ -78,12 +88,62 @@ class MainActivity : AppCompatActivity() {
         try {
             youtubeDL.init(this)
             youtubeDL.updateYoutubeDL(application, {
-                Log.d(TAG, "YoutubeDL updated")
+                Log.d("main", "YoutubeDL updated")
             }, {
                 Toasty.error(this@MainActivity, "failed to initialize the app. Things probably wont go nicely", Toast.LENGTH_LONG).show()
             })
         } catch (e: YoutubeDLException) {
             Toasty.error(this@MainActivity, "failed to initialize the app. Things probably wont go nicely", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun requestPermissions() {
+        if(!checkPermission()){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+                try {
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+                    val uri = Uri.fromParts("package", this.packageName, null)
+                    intent.data = uri
+                    storageActivityResultLauncher.launch(intent)
+                }
+                catch (e: Exception){
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION
+                    storageActivityResultLauncher.launch(intent)
+                }
+            }
+            else{
+                //Android is below 11(R)
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(permission.WRITE_EXTERNAL_STORAGE, permission.READ_EXTERNAL_STORAGE),
+                    100
+                )
+            }
+        }
+    }
+
+    private fun checkPermission(): Boolean{
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            //Android is 11(R) or above
+            Environment.isExternalStorageManager()
+        }
+        else{
+            //Android is below 11(R)
+            val write = ContextCompat.checkSelfPermission(this, permission.WRITE_EXTERNAL_STORAGE)
+            val read = ContextCompat.checkSelfPermission(this, permission.READ_EXTERNAL_STORAGE)
+            write == PackageManager.PERMISSION_GRANTED && read == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private val storageActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        //here we will handle the result of our intent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            //Android is 11(R) or above
+            if (!Environment.isExternalStorageManager()){
+                //Manage External Storage Permission is granted
+                Toasty.info(this, "Manage External Storage Permission is denied....", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
